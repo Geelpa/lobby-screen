629c45302912780293f31971029554b27790b344
geelpa <eduardo.augusto157@hotmail.com>
2022-09-01 13:02:11 -0300

    rename

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-
-
-function toggleModal() {
-    const getModal = document.querySelector(".modal")
-    if (getModal.style.display == 'none') {
-        getModal.style.display = "block";
-        getModal.style.background = "rgb(0 0 0 / 75%)";
-    } else {
-        getModal.style.display = " none"
-    }
-}
-
-
